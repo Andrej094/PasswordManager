755482598479f5bbdac1ec01226422cc81c4866a
AndrejStojanovski <andrej.stojanovski.2@students.finki.ukim.mk>
2026-04-06 18:46:23 +0200

    add open website function

--- app/gui.py
+++ app/gui.py
@@ -14,7 +14,7 @@ class PasswordManagerApp:
         self.root = root
         self.root.title("Password Manager")
         self.root.geometry("980x700")
-        self.root.minsize(940, 660)
+        self.root.minsize(980, 700)
 
         self.dark_mode = True
         self.selected_site = None
